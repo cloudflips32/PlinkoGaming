14777072c874661e2006e2f844966bbfaf320aaf
cloudflips32 <devadamstandish@gmail.com>
2025-04-20 16:17:36 -0400

    adjusting css

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
         muted
         loop
         playsInline
-        className="absolute overflow-hidden mt-auto ml-auto mr-autoleft-0 w-full h-full max-w-[360px] max-h-[700px] top-16 left-9 object-cover rounded-lg"
+        className="absolute overflow-hidden mt-auto ml-auto mr-autoleft-0 w-full h-full max-w-[360px] max-h-[700px] top-16 left-6 object-cover rounded-lg"
         src="/gaming.mp4"
       />
       <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-gradient-to-b from-black-950 via-purple-900 to-purple-900">
