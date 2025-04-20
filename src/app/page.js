fd4434d61b4ec836f4c5a6b91916f6eef7ce2b9e
cloudflips32 <devadamstandish@gmail.com>
2025-04-20 16:25:34 -0400

    another edit for mobile

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
         muted
         loop
         playsInline
-        className="absolute overflow-hidden mt-auto ml-auto mr-auto  w-full h-full max-h-[700px] top-16 left-2 object-cover rounded-lg"
+        className="absolute overflow-hidden mt-auto ml-auto mr-auto  w-full h-full max-h-[700px] top-16 object-cover rounded-lg"
         src="/gaming.mp4"
       />
       <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-gradient-to-b from-black-950 via-purple-900 to-purple-900">
